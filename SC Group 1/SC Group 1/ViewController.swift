f331e453630448cef2356176228e539a46122225
alexdneprin <Ruvoni45>
2022-08-05 23:15:08 +0100

    Added empty x-code project

---
+++ SC Group 1/SC Group 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SC Group 1
+//
+//  Created by Oleksandr Slobodianiuk on 05.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
