fce480123c49a22ad4a7b41eea9aa2dcb29c5037
Sectumsorted <deqvoo@gmail.com>
2022-08-07 21:31:39 +0300

    add personal info

--- SC Group 1/SC Group 1/ViewController.swift
+++ SC Group 1/SC Group 1/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Ruslan Liulka, Telegram: @Ukrlan_smu
+        // Dmitry Ilichov, Telegram: @sectumsorted
         // Roman -> telegram nickName - gR
 
     }
